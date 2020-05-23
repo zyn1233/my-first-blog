e61918f7e0152ec9b3024f9ccbc7e202b611733d
zyn1233 <809854538@qq.com>
2020-05-23 19:23:36 +0800

    加入PythonAnywhere网站

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o@wpbjh6ywbz)^(_v&k^$apk*f34&6gi!l@^yxrzs3sm!-=qv)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zyn1233.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
