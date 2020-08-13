6b69d544617ccc42ff14e350520dc8578a29602b
zyn1233 <809854538@qq.com>
2020-08-13 16:41:39 +0800

    重新构建网站

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
-
+# Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='作者')
+    title = models.CharField(max_length=200, verbose_name='标题')
+    text = models.TextField(verbose_name='内容')
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=timezone.now, verbose_name='创建时间')
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            blank=True, null=True, verbose_name='发布时间')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,3 +17,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = '池塘'
+        verbose_name_plural = '我的博客'
