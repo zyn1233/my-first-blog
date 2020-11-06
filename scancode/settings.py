793857f4f379731f1bf342e600d4bfc723d77e1e
zyn1233 <809854538@qq.com>
2020-11-06 11:04:25 +0800

    修改数据库为自带的

--- scancode/settings.py
+++ scancode/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'scancode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mina_scancode',
-        'USER': 'zyn1233',  # 你的数据库用户名
-        'PASSWORD': 'zhanggwg1233',  # 你的数据库密码
-        'HOST': '',  # 你的数据库主机，留空默认为localhost
-        'PORT': '3306',  # 你的数据库端口
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mina_scancode',
+#         'USER': 'zyn1233',  # 你的数据库用户名
+#         'PASSWORD': 'zhanggwg1233',  # 你的数据库密码
+#         'HOST': '',  # 你的数据库主机，留空默认为localhost
+#         'PORT': '3306',  # 你的数据库端口
+#     }
+# }
 
 
 # Password validation
